4907aca85847dd3057f4a526a5b05f810272f9ba
Albert Costas <acostasg@uoc.edu>
2017-04-26 23:14:15 +0200

    Fixed test from route session token

--- src/test/java/api/SessionTest.java
+++ src/test/java/api/SessionTest.java
@@ -9,10 +9,11 @@ public class SessionTest extends AbstractTest {
     @Test
     public void test_login() {
         String responseMsg = this.target.path("session/token")
+                .queryParam("token","sakjfh97325437hskfsdfd_sdkjfsjf1283763339564921734sdfbdsj")
                 .request()
                 .header("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9")
                 .get(String.class);
-        assertEquals("Success!", responseMsg);
+        assertEquals("{\"Token\":{\"hash\":\"sakjfh97325437hskfsdfd_sdkjfsjf1283763339564921734sdfbdsj\"}}", responseMsg);
     }
 
 }
--- src/test/java/api/UserTest.java
+++ src/test/java/api/UserTest.java
@@ -18,7 +18,7 @@ public class UserTest extends AbstractTest {
                 .request()
                 .header("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9")
                 .get(String.class);
-        assertEquals("{\"Token\":{\"hash\":\"dummyToken\"}}", responseMsg);
+        assertEquals("{\"Token\":{\"hash\":\"sakjfh97325437hskfsdfd_sdkjfsjf1283763339564921734sdfbdsj\"}}", responseMsg);
     }
 
     @Test
